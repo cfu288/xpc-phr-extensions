1a9f34bd255b381443bf93d383ba7ee87e6341b8
Christopher Fu <invent288cfu@gmail.com>
2023-06-10 15:31:01 -0400

    feat: add working code for openAPI but don't use it for demo - too slow

--- features/cds_services_discovery_handler/cds_services_discovery_handler.py
+++ features/cds_services_discovery_handler/cds_services_discovery_handler.py
@@ -8,5 +8,11 @@ class CDSServicesDiscoveryHandler:
                     "description": "Checks your recent A1C and provides education resources via Sanctuary Health",
                     "id": "sanctuary-health-diabetes-education",
                 },
+                {
+                    "hook": "diagnostic-report-open",
+                    "title": "Diabetes AI Advisor - Powered by OpenAI",
+                    "description": "Have ChatGPT give you insights about your A1C values",
+                    "id": "diabetes-ai-advisor",
+                },
             ]
         }
--- features/diabetes_ai_advisor/diabetes_ai_advisor_handler.py
+++ features/diabetes_ai_advisor/diabetes_ai_advisor_handler.py
@@ -4,6 +4,8 @@ from features.diabetes_ai_advisor.models.diabetes_ai_advisor_request import (
     DiabetesAIAdvisorRequest,
 )
 import time
+import openai
+import os
 
 
 class DiabetesAIAdvisorHandler:
@@ -11,7 +13,6 @@ class DiabetesAIAdvisorHandler:
         pass
 
     def handle(self, req: DiabetesAIAdvisorRequest) -> Response:
-        print(req)
         # in reality, we would use the req with the users fhirSources.fhirServer, fhirSources.fhirAuthorization
         # and context.diagnosticReportId to query for the relevant diagnostic report data, see if any of the data contains
         # an A1C value, and if so, return the appropriate response. Otherwise, return an empty response.
@@ -36,24 +37,66 @@ class DiabetesAIAdvisorHandler:
         if req.contexts.get(fhirService.fhirServer).diagnosticReportId in set(
             ["TeOs2UvPrb0X2j3quRbvpAnIv8HVpc3MfPHsTs5MXbxQB"]
         ):
+            print(os.getenv("OPEN_API_KEY"))
+            openai.api_key = os.getenv("OPEN_API_KEY")
+            # models = openai.Model.list()
+            # print the first model's id
+            # print(models.data)
+            # chat_completion = openai.ChatCompletion.create(
+            #     model="gpt-3.5-turbo-0301",
+            #     messages=[
+            #         {
+            #             "role": "assistant",
+            #             "content": """
+            #             Year	A1c
+            #             2020	4.5
+            #             2021	5
+            #             2022	5.3
+            #             2023	5.8
+            #             This are A1c lab values for a patient. Patient has an IQ of 120 and needs simple english explanation of what this means for them and recommendations as TLDR. Pretend you are talking to them directly.
+
+            #             Pleae format the response in HTML with inline styling. You can use the following template:
+            #             <p>Your A1c values have been increasing over the years, indicating a slight decline in blood sugar control. A higher A1c means poorer blood sugar control. You should:</p>
+            #             <ul>
+            #                 <li style="margin-left: 20px; list-style-type: circle;">Monitor blood sugar levels regularly.</li>
+            #                 <li style="margin-left: 20px; list-style-type: circle;">Review your diabetes management plan and make adjustments if necessary.</li>
+            #                 <li style="margin-left: 20px; list-style-type: circle;">Focus on healthy lifestyle habits like balanced eating, exercise, sleep, stress management, and maintaining a healthy weight.</li>
+            #             </ul>
+            #             </br>
+            #             """,
+            #         }
+            #     ],
+            # )
+            # print(chat_completion)
             time.sleep(2)
             return [
                 {
                     "summary": "Diabetes AI Advisor - Powered by OpenAI",
+                    # "detail": chat_completion.choices[0].text,
                     "detail": """Your A1c values have been increasing over the years, indicating a slight decline in blood sugar control. A higher A1c means poorer blood sugar control. You should:
                     <ul>
-                        <li> - Monitor blood sugar levels regularly.</li>
-                        <li> - Review your diabetes management plan and make adjustments if necessary.</li>
-                        <li> - Focus on healthy lifestyle habits like balanced eating, exercise, sleep, stress management, and maintaining a healthy weight.</li>
+                        <li style="margin-left: 20px; list-style-type: circle;">Monitor blood sugar levels regularly.</li>
+                        <li style="margin-left: 20px; list-style-type: circle;">Review your diabetes management plan and make adjustments if necessary.</li>
+                        <li style="margin-left: 20px; list-style-type: circle;">Focus on healthy lifestyle habits like balanced eating, exercise, sleep, stress management, and maintaining a healthy weight.</li>
                     </ul>
                     </br>
                     <div style="border-bottom: 1px solid #000;"></div>
                     </br>
                     <b>Below are the resources available to you:</b>
                     </br>
-                    <b>Virta Health</b> - a digital solution to reverses type 2 diabetes and pre-diabetes. <a style="color:blue" href="https://www.virtahealth.com/">Learn more</a>
+                    <table>
+                    <thead>
+                    <tr>
+                        <td style="width: 100px"><img src="https://files.mari.casa/virta.png" alt="Virta Health" style="width: 80px"></img></td>
+                        <td>A digital solution to reverses type 2 diabetes and pre-diabetes. <a style="color:blue" href="https://www.virtahealth.com/">Learn more</a></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 100px"><img src="https://files.mari.casa/livongo.png" alt="Livongo" style="width: 80px"></img></td>
+                        <td>Smart management of diabetes with personalized tips and expert coaching. <a style="color:blue" href="https://www.virtahealth.com/">Learn more</a></td>
+                    </tr>
+                    </thead>
+                    </table>
                     </br>
-                    <b>Livongo</b> - smart management of diabetes with personalized tips and expert coaching. <a style="color:blue" href="https://www.virtahealth.com/">Learn more</a>
                     """,
                     "indicator": "info",
                     "source": {
